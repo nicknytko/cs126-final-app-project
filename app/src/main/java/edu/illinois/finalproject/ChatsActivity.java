066039a3de655bf32260e5eaf5d9ef80724149cd
Nicolas Nytko <nicknytko@gmail.com>
2017-12-04 20:58:32 -0600

    chats list activity

--- app/src/main/java/edu/illinois/finalproject/ChatsActivity.java
+++ app/src/main/java/edu/illinois/finalproject/ChatsActivity.java
@@ -3,7 +3,7 @@ package edu.illinois.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChatsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
