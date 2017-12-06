3462174c21daedaf78f63ffc92293689a3b8c78d
Nicolas Nytko <nicknytko@gmail.com>
2017-12-06 15:56:05 -0600

    implement chat bubble layouts

---
+++ app/src/main/java/edu/illinois/finalproject/MessagesActivity.java
@@ -0,0 +1,15 @@
+package edu.illinois.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nicolas Nytko on 12/6/17.
+ */
+
+public class MessagesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
